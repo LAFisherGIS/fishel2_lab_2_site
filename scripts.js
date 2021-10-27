e2cba0779a73087e9960df2a1bbe69a453d53bb4
LAFisherGIS <fishel2@uw.edu>
2021-10-27 11:14:27 -0700

    Improved symbology

--- scripts.js
+++ scripts.js
@@ -39,12 +39,13 @@ function mapping(){
   $.getJSON("Data/Contaminated_Sites_(Ecology).geojson",function(data){
      L.geoJson(data, {
        pointToLayer: function(feature, latlng){
-         var riskColor,
-         risk = feature.properties.SiteRank;
-          if (feature.properties.SiteRank === "1 - Highest Assessed Risk") riskColor = 'red';
+        var riskColor,
+        risk = feature.properties.SiteRank;
+          if (feature.properties.SiteRank === "0 - NPL Site (Fed HRS Score)") riskColor = 'maroon';
+          else if (feature.properties.SiteRank === "1 - Highest Assessed Risk") riskColor = 'red';
           else if (feature.properties.SiteRank === "2 - Moderate-High Risk") riskColor = 'orangered';
           else if (feature.properties.SiteRank === "3 - Moderate Risk") riskColor = 'orange';
-          else if (feature.properties.SiteRank === "4 - Low-Moderate Risk") riskColor = 'yelloworange';
+          else if (feature.properties.SiteRank === "4 - Low-Moderate Risk") riskColor = 'goldenrod';
           else if (feature.properties.SiteRank === "5 - Lowest Assessed Risk") riskColor = 'yellow';
          var marker = L.circle(latlng, {radius: 200, color: riskColor});
          if (feature.properties.SiteRank !== "")
@@ -53,10 +54,19 @@ function mapping(){
      }).addTo(mymap);
   });
 
+  L.MakiMarkers.accessToken = "sk.eyJ1IjoibGFmaXNoZXJnaXMiLCJhIjoiY2t2OXJ4dnV1YTY2ZjJwbnpjM3BxbWRnYiJ9.CW4oaT94TkbelBF0Fj4rJw";
+
+  const schoolIcon = L.MakiMarkers.icon({
+    icon: "school",
+    color: "006400",
+    size: "s"
+  });
+
   $.getJSON("Data/Schools.geojson",function(data){
      L.geoJson(data, {
        pointToLayer: function(feature, latlng){
-         var marker = L.circle(latlng, {radius: 25, color: 'green', fillColor: 'green', fillOpacity:1});
+         var marker = L.marker(latlng, {icon: schoolIcon});
+         marker.bindPopup("Name: " + feature.properties.NAME + "<br>Address: " + feature.properties.ADDRESS + "<br> School Type: " + feature.properties.TYPE)
          if (feature.properties.CITY === "TACOMA")
          return marker;
        }
