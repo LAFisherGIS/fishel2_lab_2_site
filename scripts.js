302c0c0da3e0ef2dfddc793abd8065bf16c3e0ad
LAFisherGIS <fishel2@uw.edu>
2021-10-26 19:41:41 -0700

    Attempted to fix tri data

--- scripts.js
+++ scripts.js
@@ -13,20 +13,26 @@ function modeSet(){
   localStorage.setItem("status", checked);
 }
 
+// End Tero-based scripting
+
 function mapping(){
   var mymap = L.map('map').setView([47.2528769, -122.4442906], 12);
   var basemap = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.png', {
 			attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 		}).addTo(mymap);
+
   $.getJSON("Data/Neighborhood_Council_Districts_(Tacoma).geojson",function(data){
      L.geoJson(data).addTo(mymap);
   });
+
   $.getJSON("Data/School_Grounds.geojson",function(data){
      L.geoJson(data).addTo(mymap);
   });
+
   $.getJSON("Data/Schools.geojson",function(data){
      L.geoJson(data).addTo(mymap);
   });
+
   $.getJSON("Data/tri_2020_pierce.geojson",function(data){
      L.geoJson(data).addTo(mymap);
   });
