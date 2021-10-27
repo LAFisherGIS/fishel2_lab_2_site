f09c7e703b3a68ae98d9740ee39169f6f67c721a
LAFisherGIS <fishel2@uw.edu>
2021-10-26 18:20:28 -0700

    Created initial repo

---
+++ scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(modeCheck);
+$(document).ready(mapping);
+
+/* This code is based on Tero Karvinen's reference implementation (https://terokarvinen.com/2018/save-checkbox-state-to-localstorage-javascript-and-jquery-example/) for the use of localStorage to preserve the state of a checkbox between pages and sessions. */
+function modeCheck(){
+  let checked="true"==localStorage.getItem("status");
+  $("#lightordark").prop('checked', checked)
+  $("#lightordark").click(modeSet);
+}
+
+function modeSet(){
+  let checked=$("#lightordark").is(":checked");
+  localStorage.setItem("status", checked);
+}
+
+function mapping(){
+  var mymap = L.map('map').setView([47.2528769, -122.4442906], 12);
+  var basemap = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.png', {
+			attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		}).addTo(mymap);
+  $.getJSON("Data/Neighborhood_Council_Districts_(Tacoma).geojson",function(data){
+     L.geoJson(data).addTo(mymap);
+  });
+  $.getJSON("Data/School_Grounds.geojson",function(data){
+     L.geoJson(data).addTo(mymap);
+  });
+  $.getJSON("Data/Schools.geojson",function(data){
+     L.geoJson(data).addTo(mymap);
+  });
+  $.getJSON("Data/tri_2020_pierce.geojson",function(data){
+     L.geoJson(data).addTo(mymap);
+  });
+}
